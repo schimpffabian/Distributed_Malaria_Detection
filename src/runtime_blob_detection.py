40a5f78b780381f37e564ec4687f14a50bb6c3e9
fasc <schimpfabian@gmail.com>
2019-05-14 17:04:02 +0200

    added runtime test blob detection

--- src/runtime_blob_detection.py
+++ src/runtime_blob_detection.py
@@ -40,7 +40,7 @@ def get_accuracy(kd_tree, blob_list, center_list, threshold):
         return [0, 1, 0]
 
 
-def compare_blob_detection(num_runs=1, threshold=4):
+def compare_blob_detection(num_runs=15, threshold=4):
     """
     Compare speed and detection rate of different standard tools
 
@@ -123,9 +123,9 @@ def compare_blob_detection(num_runs=1, threshold=4):
 
     # Average Time over runs
     log_time = np.sum(log_time) / num_runs
-    dog_time =  np.sum(dog_time) / num_runs
-    doh_time =  np.sum(doh_time) / num_runs
-    sbd_time =  np.sum(sbd_time) / num_runs
+    dog_time = np.sum(dog_time) / num_runs
+    doh_time = np.sum(doh_time) / num_runs
+    sbd_time = np.sum(sbd_time) / num_runs
 
     # Convert arrays to numpy
     log_acc = np.array(log_acc)
@@ -155,11 +155,42 @@ def compare_blob_detection(num_runs=1, threshold=4):
         % (sbd_time, np.mean(sbd_acc[:, 0]), np.mean(sbd_acc[:, 2]))
     )
 
-    save_list = [log_time, log_acc, dog_time, dog_acc,
-                 doh_time, doh_acc, sbd_acc, sbd_acc]
+    save_list = np.array([log_time, dog_time, doh_time, sbd_time])
 
-    np.savetxt(Path("./logs/runtime_blob_detection.csv"), save_list, delimiter=",", header="log_time,log_acc,dog_time,dog_acc,"
-                                                        "doh_time, doh_acc, sbd_acc, sbd_acc")
+    np.savetxt(
+        Path("./logs/runtime_blob_detection.csv"),
+        save_list,
+        delimiter=",",
+        header="log_time,log_acc,dog_time,dog_acc,"
+        "doh_time, doh_acc, sbd_acc, sbd_acc",
+    )
+    np.savetxt(
+        Path("./logs/log_acc_blob_detection.csv"),
+        log_acc,
+        delimiter=",",
+        header="true_positive_rate, false_positive_rate, detected_centers_rate",
+    )
+
+    np.savetxt(
+        Path("./logs/dog_acc_blob_detection.csv"),
+        dog_acc,
+        delimiter=",",
+        header="true_positive_rate, false_positive_rate, detected_centers_rate",
+    )
+
+    np.savetxt(
+        Path("./logs/doh_acc_blob_detection.csv"),
+        doh_acc,
+        delimiter=",",
+        header="true_positive_rate, false_positive_rate, detected_centers_rate",
+    )
+
+    np.savetxt(
+        Path("./logs/sbd_acc_blob_detection.csv"),
+        sbd_acc,
+        delimiter=",",
+        header="true_positive_rate, false_positive_rate, detected_centers_rate",
+    )
 
 
 if __name__ == "__main__":
